95e9ddb60e64d746ccc9ae4705bd165815095971
pwt <1089749+pwt@users.noreply.github.com>
2022-09-20 18:22:53 +0100

    Consolidate JSON/non-JSON submission code

--- scripts/submit.py
+++ scripts/submit.py
@@ -5,10 +5,10 @@ A minimal, example script to submit a Bash script task. No error-checking is
 performed.
 """
 
-from json import load
+from json import JSONDecodeError, load
 from math import ceil
 from pathlib import Path
-from typing import List
+from typing import Dict, List, Optional
 
 from yellowdog_client import PlatformClient
 from yellowdog_client.model import (
@@ -49,21 +49,24 @@ ID = generate_id(CONFIG_COMMON.name_tag + "_Task")
 
 TASK_BATCH_SIZE = 2000
 
-INPUT_FOLDER_NAME = "INPUT"
+INPUT_FOLDER_NAME = "INPUTS"
 
 
 def main():
     print_log(f"ID = {ID}")
-    if CONFIG_WR.tasks_data_file is not None:
-        _submit_work_requirement_from_json()
-    else:
-        if CONFIG_WR.bash_script not in CONFIG_WR.input_files:
-            CONFIG_WR.input_files.append(CONFIG_WR.bash_script)
-        for file in CONFIG_WR.input_files:
-            upload_file(file)
-        submit_tasks()
-    CLIENT.close()
-    print_log("Done")
+    try:
+        if CONFIG_WR.tasks_data_file is not None:
+            with open(CONFIG_WR.tasks_data_file, "r") as f:
+                tasks_data = load(f)
+            _submit_work_requirement(tasks_data=tasks_data)
+        else:
+            task_count = CONFIG_WR.task_count
+            _submit_work_requirement(task_count=task_count)
+        print_log("Done")
+    except (JSONDecodeError, FileNotFoundError) as e:
+        print_log(f"Error: {e}")
+    finally:
+        CLIENT.close()
 
 
 def upload_file(filename: str):
@@ -92,85 +95,6 @@ def upload_file(filename: str):
         print_log(f"Uploaded file '{filename}' to YDOS: {link_}")
 
 
-def submit_tasks():
-    """
-    Creates the YD Task Group and Work Requirement, and adds the Tasks to be
-    executed.
-    """
-    task_group_name = "OUTPUT"
-    task_name_prefix = "TASK_"
-
-    # Define the properties of the Task Group
-    run_specification = RunSpecification(
-        taskTypes=[CONFIG_WR.task_type],
-        maximumTaskRetries=CONFIG_WR.max_retries,
-        workerTags=CONFIG_WR.worker_tags,
-    )
-    # Create the Task Group object
-    task_group = TaskGroup(name=task_group_name, runSpecification=run_specification)
-    # Create the Work Requirement containing the Task Group
-    work_requirement = CLIENT.work_client.add_work_requirement(
-        WorkRequirement(
-            namespace=CONFIG_COMMON.namespace,
-            name=ID,
-            taskGroups=[task_group],
-            tag=CONFIG_COMMON.name_tag,
-        )
-    )
-    print_log(f"Added {link_entity(CONFIG_COMMON.url, work_requirement)}")
-    # Define the Task for running the Bash script
-    input_files = [
-        TaskInput.from_task_namespace(unique_upload_pathname(file), required=True)
-        for file in CONFIG_WR.input_files
-    ]
-    output_files = [
-        TaskOutput.from_worker_directory(output_file)
-        for output_file in CONFIG_WR.output_files
-    ]
-    # Add the console output file
-    output_files.append(TaskOutput.from_task_process())
-    arguments_list = [unique_upload_pathname(CONFIG_WR.bash_script)] + CONFIG_WR.args
-    # Determine batching of Tasks if required
-    num_task_batches: int = ceil(CONFIG_WR.task_count / TASK_BATCH_SIZE)
-    if num_task_batches > 1:
-        print_log(
-            "Adding Tasks to Work Requirement Task Group in "
-            f"{num_task_batches} batches"
-        )
-    zfill_len = len(str(CONFIG_WR.task_count))
-    for batch_number in range(num_task_batches):
-        task_list = []
-        for task_number in range(
-            (TASK_BATCH_SIZE * batch_number) + 1,
-            min(TASK_BATCH_SIZE * (batch_number + 1), CONFIG_WR.task_count) + 1,
-        ):
-            task_name_numbered = task_name_prefix + str(task_number).zfill(zfill_len)
-            task_list.append(
-                Task(
-                    name=task_name_numbered,
-                    taskType=CONFIG_WR.task_type,
-                    inputs=input_files,
-                    arguments=arguments_list,
-                    environment=CONFIG_WR.env,
-                    outputs=output_files,
-                )
-            )
-        # Add the Tasks to the Task Group
-        CLIENT.work_client.add_tasks_to_task_group_by_name(
-            CONFIG_COMMON.namespace, work_requirement.name, task_group_name, task_list
-        )
-        if num_task_batches > 1:
-            print_log(
-                f"Batch {str(batch_number + 1).zfill(len(str(num_task_batches)))} : "
-                f"Added {len(task_list):,d} "
-                f"Task(s) to Work Requirement Task Group '{task_group_name}'"
-            )
-    print_log(
-        f"Added {CONFIG_WR.task_count} Task(s) to Work Requirement Task Group "
-        f"'{task_group_name}'"
-    )
-
-
 def unique_upload_pathname(filename: str, urlencode_forward_slash: bool = False) -> str:
     """
     Maps the local filename into a uniquely identified upload object
@@ -182,17 +106,13 @@ def unique_upload_pathname(filename: str, urlencode_forward_slash: bool = False)
     return ID + forward_slash + INPUT_FOLDER_NAME + forward_slash + filename
 
 
-def _submit_work_requirement_from_json():
+def _submit_work_requirement(
+    tasks_data: Optional[Dict] = None, task_count: Optional[int] = None
+):
     """
-    Submit a Work Requirement defined in a JSON file.
+    Submit a Work Requirement defined in a tasks_data dictionary.
     """
-    # Load data
-    with open(CONFIG_WR.tasks_data_file, "r") as f:
-        try:
-            tasks_data = load(f)
-        except Exception as e:
-            print_log(f"Error loading JSON tasks data: {e}")
-            return
+    tasks_data = {TASK_GROUPS: [{TASKS: [{}]}]} if tasks_data is None else tasks_data
 
     num_task_groups = len(tasks_data[TASK_GROUPS])
 
@@ -252,8 +172,9 @@ def _submit_work_requirement_from_json():
     # Add Tasks to their Task Groups
     for tg_number, task_group in enumerate(task_groups):
         tasks = tasks_data[TASK_GROUPS][tg_number][TASKS]
-        num_tasks = len(tasks)
+        num_tasks = len(tasks) if task_count is None else task_count
         num_task_batches: int = ceil(num_tasks / TASK_BATCH_SIZE)
+        tasks_list: List[Task] = []
         if num_task_batches > 1:
             print_log(
                 f"Adding Tasks to Task Group {task_group.name} in "
@@ -266,7 +187,7 @@ def _submit_work_requirement_from_json():
                 min(TASK_BATCH_SIZE * (batch_number + 1), num_tasks),
             ):
                 task_group_data = tasks_data[TASK_GROUPS][tg_number]
-                task = tasks[task_number]
+                task = tasks[task_number] if task_count is None else tasks[0]
                 task_name = task.get(
                     NAME, "Task_" + str(task_number + 1).zfill(len(str(num_tasks)))
                 )
@@ -313,7 +234,13 @@ def _submit_work_requirement_from_json():
                 task_group.name,
                 tasks_list,
             )
-            print_log(f"Added {len(tasks_list)} Tasks to Task Group {task_group.name}")
+            if num_task_batches > 1:
+                print_log(
+                    f"Batch {str(batch_number + 1).zfill(len(str(num_task_batches)))} : "
+                    f"Added {len(tasks_list):,d} "
+                    f"Task(s) to Work Requirement Task Group {task_group.name}"
+                )
+        print_log(f"Added {len(tasks_list)} Tasks to Task Group {task_group.name}")
 
 
 # Entry point
