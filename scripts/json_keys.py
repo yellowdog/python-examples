f8053e9746121c5e48dc9f039562c44b234089f0
pwt <1089749+pwt@users.noreply.github.com>
2022-09-26 11:07:23 +0100

    Remove redundant 'BASH_SCRIPT' json key

--- scripts/json_keys.py
+++ scripts/json_keys.py
@@ -4,7 +4,6 @@ Strings used for keys in the JSON input file.
 
 ARGS = "args"
 AUTO_FAIL = "auto_fail"
-BASH_SCRIPT = "bash_script"
 COMPLETED_TASK_TTL = "completed_task_ttl"
 DEPENDENT_ON = "dependent_on"
 ENV = "env"
