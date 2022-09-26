5bf3a80af93baa13c50749c9cb5fbadc31dc3739
pwt <1089749+pwt@users.noreply.github.com>
2022-09-26 11:14:01 +0100

    Add type documentation to JSON strings

--- scripts/json_keys.py
+++ scripts/json_keys.py
@@ -1,30 +1,30 @@
 """
-Strings used for keys in the JSON input file.
+Strings used as keys in the JSON input file.
 """
 
-ARGS = "args"
-AUTO_FAIL = "auto_fail"
-COMPLETED_TASK_TTL = "completed_task_ttl"
-DEPENDENT_ON = "dependent_on"
-ENV = "env"
-EXCLUSIVE_WORKERS = "exclusive_workers"
-EXECUTABLE = "executable"
-FULFIL_ON_SUBMIT = "fulfil_on_submit"
-INPUT_FILES = "input_files"
-INSTANCE_TYPES = "instance_types"
-MAX_RETRIES = "max_retries"
-MAX_WORKERS = "max_workers"
-MIN_WORKERS = "min_workers"
-NAME = "name"
-OUTPUT_FILES = "output_files"
-PRIORITY = "priority"
-PROVIDERS = "providers"
-RAM = "ram"
-REGIONS = "regions"
-TASKS = "tasks"
-TASKS_PER_WORKER = "tasks_per_worker"
-TASK_GROUPS = "task_groups"
-TASK_TYPE = "task_type"
-TASK_TYPES = "task_types"
-VCPUS = "vcpus"
-WORKER_TAGS = "worker_tags"
+ARGS = "args"  # List
+AUTO_FAIL = "auto_fail"  # Boolean
+COMPLETED_TASK_TTL = "completed_task_ttl"  # Float
+DEPENDENT_ON = "dependent_on"  # String
+ENV = "env"  # Dictionary
+EXCLUSIVE_WORKERS = "exclusive_workers"  # Boolean
+EXECUTABLE = "executable"  # String
+FULFIL_ON_SUBMIT = "fulfil_on_submit"  # Boolean
+INPUT_FILES = "input_files"  # List of Strings
+INSTANCE_TYPES = "instance_types"  # List of Strings
+MAX_RETRIES = "max_retries"  # Integer
+MAX_WORKERS = "max_workers"  # Integer
+MIN_WORKERS = "min_workers"  # Integer
+NAME = "name"  # String
+OUTPUT_FILES = "output_files"  # List of Strings
+PRIORITY = "priority"  # Float
+PROVIDERS = "providers"  # List of Strings
+RAM = "ram"  # List of two Floats
+REGIONS = "regions"  # List of Strings
+TASKS = "tasks"  # List of Tasks
+TASKS_PER_WORKER = "tasks_per_worker"  # Integer
+TASK_GROUPS = "task_groups"  # List of Task Groups
+TASK_TYPE = "task_type"  # String
+TASK_TYPES = "task_types"  # List of Strings
+VCPUS = "vcpus"  # List of two Floats
+WORKER_TAGS = "worker_tags"  # List of Strings
