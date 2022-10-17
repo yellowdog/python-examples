0b84369ef67c45b862d9d0f7ed2695ce799efb79
pwt <1089749+pwt@users.noreply.github.com>
2022-10-17 22:14:27 +0100

    Expand 'which_config' reporting

--- scripts/which_config.py
+++ scripts/which_config.py
@@ -6,12 +6,29 @@ will be used.
 """
 
 from common import (
+    ConfigCommon,
+    ConfigWorkerPool,
+    ConfigWorkRequirement,
     load_config_common,
     load_config_work_requirement,
     load_config_worker_pool,
+    print_log,
 )
 
-load_config_common()
-load_config_work_requirement()
-load_config_worker_pool()
-exit(0)
+
+def main():
+    config_common: ConfigCommon = load_config_common()
+    print_log(f"namespace = '{config_common.namespace}'")
+    print_log(f"name_tag = '{config_common.name_tag}'")
+
+    config_wr: ConfigWorkRequirement = load_config_work_requirement()
+    if config_wr.tasks_data_file is not None:
+        print_log(f"work requirement data file = '{config_wr.tasks_data_file}'")
+
+    config_wp: ConfigWorkerPool = load_config_worker_pool()
+    if config_wp.worker_pool_data_file is not None:
+        print_log(f"worker pool data file = '{config_wp.worker_pool_data_file}'")
+
+
+if __name__ == "__main__":
+    main()
