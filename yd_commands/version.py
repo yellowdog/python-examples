c307a0c869985d6bdf43e5e0daf53c098daa5967
pwt <1089749+pwt@users.noreply.github.com>
2022-11-24 10:10:00 +0000

    Add Python executable path to the 'yd-version' output

--- yd_commands/version.py
+++ yd_commands/version.py
@@ -4,6 +4,7 @@
 Report version numbers
 """
 
+from sys import executable
 from sys import version as python_version
 
 from yellowdog_client._version import __version__ as yd_sdk_version
@@ -14,7 +15,7 @@ from yd_commands.__init__ import __version__
 def main():
     print(f" Python Examples Version:   {__version__}")
     print(f" YellowDog SDK Version:     {yd_sdk_version}")
-    print(f" Python Version:            {python_version.split()[0]}")
+    print(f" Python Version:            {python_version.split()[0]} ({executable})")
 
 
 if __name__ == "__main__":
