80eaded7e7228366c63a4fe391ac446f900f43dc
pwt <1089749+pwt@users.noreply.github.com>
2022-11-25 13:25:11 +0000

    Cosmetic changes to 'yd-version'

--- yd_commands/version.py
+++ yd_commands/version.py
@@ -13,9 +13,12 @@ from yd_commands.__init__ import __version__
 
 
 def main():
-    print(f" Python Examples Version:   {__version__}")
-    print(f" YellowDog SDK Version:     {yd_sdk_version}")
-    print(f" Python Version:            {python_version.split()[0]} ({executable})")
+    print(f" YellowDog Python Examples Version:   {__version__}")
+    print(f" YellowDog SDK Version:               {yd_sdk_version}")
+    print(
+        f" Python Version:                      {python_version.split()[0]} "
+        f"({executable})"
+    )
 
 
 if __name__ == "__main__":
