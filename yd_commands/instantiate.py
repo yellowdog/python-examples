d3d07b297decb4581f169d1e0a296aac1850da08
pwt <1089749+pwt@users.noreply.github.com>
2022-12-09 19:27:16 +0000

    Tidy up naming, etc., in yd-instantiate

--- yd_commands/instantiate.py
+++ yd_commands/instantiate.py
@@ -20,10 +20,10 @@ from yd_commands.printing import print_error, print_log
 from yd_commands.wrapper import CLIENT, CONFIG_COMMON, main_wrapper
 
 
-# Specifies the cardinality for a Compute Requirement batch
+# Specifies the number of instances in a Compute Requirement batch
 @dataclass
-class WPBatch:
-    initial_nodes: int
+class CRBatch:
+    target_instances: int
 
 
 CONFIG_WP: ConfigWorkerPool = load_config_worker_pool()
@@ -44,7 +44,7 @@ def create_compute_requirement():
         f"Provisioning Compute Requirement with {CONFIG_WP.initial_nodes:,d} "
         "instance(s)"
     )
-    batches: List[WPBatch] = _allocate_nodes_to_batches(
+    batches: List[CRBatch] = _allocate_nodes_to_batches(
         CONFIG_WP.compute_requirement_batch_size,
         CONFIG_WP.initial_nodes,
     )
@@ -58,7 +58,7 @@ def create_compute_requirement():
         if num_batches > 1:
             print_log(
                 f"Provisioning Compute Requirement {batch_number + 1} '{id}'"
-                f"with {batches[batch_number].initial_nodes:,d} instance(s)"
+                f"with {batches[batch_number].target_instances:,d} instance(s)"
             )
         else:
             print_log(f"Provisioning Compute Requirement '{id}'")
@@ -69,7 +69,7 @@ def create_compute_requirement():
                         templateId=CONFIG_WP.template_id,
                         requirementNamespace=CONFIG_COMMON.namespace,
                         requirementName=id,
-                        targetInstanceCount=batches[batch_number].initial_nodes,
+                        targetInstanceCount=batches[batch_number].target_instances,
                         requirementTag=CONFIG_COMMON.name_tag,
                     )
                 )
@@ -84,7 +84,7 @@ def create_compute_requirement():
 
 def _allocate_nodes_to_batches(
     max_batch_size: int, initial_nodes: int
-) -> List[WPBatch]:
+) -> List[CRBatch]:
     """
     Helper function to distribute the number of requested instances
     as evenly as possible over Compute Requirements when batches are required.
@@ -92,21 +92,23 @@ def _allocate_nodes_to_batches(
     num_batches = ceil(initial_nodes / max_batch_size)
     nodes_per_batch = floor(initial_nodes / num_batches)
 
-    # First pass population of batches with equal numbers
+    # First pass population of batches with equal number of instances
     batches = [
-        WPBatch(
-            initial_nodes=nodes_per_batch,
+        CRBatch(
+            target_instances=nodes_per_batch,
         )
         for _ in range(num_batches)
     ]
-    # Allocate remainders across batches
+
+    # Allocate remainder across batches
     remainder_nodes = initial_nodes - (nodes_per_batch * num_batches)
     for batch in batches:
         if remainder_nodes > 0:
-            batch.initial_nodes += 1
+            batch.target_instances += 1
             remainder_nodes -= 1
         else:
             break
+
     return batches
 
 
