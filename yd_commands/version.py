a1d668433761ab8102539b4fc79183f795e0e552
pwt <1089749+pwt@users.noreply.github.com>
2022-12-11 09:49:30 +0000

    Reformat 'yd-version' output

--- yd_commands/version.py
+++ yd_commands/version.py
@@ -5,7 +5,7 @@ Report version numbers
 """
 
 from sys import executable
-from sys import version as python_version
+from sys import version as py_version
 
 from yellowdog_client._version import __version__ as yd_sdk_version
 
@@ -15,10 +15,8 @@ from yd_commands.__init__ import __version__
 def main():
     print(f" YellowDog Python Examples Version:   {__version__}")
     print(f" YellowDog SDK Version:               {yd_sdk_version}")
-    print(
-        f" Python Version:                      {python_version.split()[0]} "
-        f"({executable})"
-    )
+    print(f" Python Version:                      {py_version.split()[0]} ")
+    print(f" Python Executable:                   {executable}")
 
 
 if __name__ == "__main__":
