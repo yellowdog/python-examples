fb8123df2237f546a9b6bb82c5ee8a9066c4209e
pwt <1089749+pwt@users.noreply.github.com>
2022-12-11 10:02:55 +0000

    Add note about 'jsonnet-binary' package

--- yd_commands/check_imports.py
+++ yd_commands/check_imports.py
@@ -5,11 +5,13 @@ Handle optional imports.
 
 def check_jsonnet_import():
     # Jsonnet is not installed by default, due to a binary build requirement
-    # on some platforms.
+    # on some platforms. The 'jsonnet-binary' package can be used to overcome
+    # the requirement for build tools to be installed.
     try:
         from _jsonnet import evaluate_file
     except ImportError:
         raise Exception(
             "The 'jsonnet' package is not installed by default; "
-            "it can be installed using 'pip install jsonnet'"
+            "it can be installed using 'pip install jsonnet' "
+            "or (if that fails) try 'pip install jsonnet-binary'"
         )
