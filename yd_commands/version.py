6498609bd295c3b7ead2e22fff6dc647091ec846
pwt <1089749+pwt@users.noreply.github.com>
2023-01-25 14:49:54 +0000

    Suppress detail in yd-version unless ''--debug' option is used

--- yd_commands/version.py
+++ yd_commands/version.py
@@ -5,6 +5,7 @@ Report version numbers, etc.
 """
 
 from os.path import abspath
+from sys import argv
 from sys import executable, path
 from sys import version as py_version
 
@@ -29,10 +30,11 @@ def main():
     print(f"  Jsonnet Support:                     {check_for_jsonnet()}")
     print(f"  YellowDog SDK Version:               {yd_sdk_version}")
     print(f"  Python Version:                      {py_version.split()[0]} ")
-    print(f"  Command:                             {abspath(__file__)}")
-    print(f"  Python Executable:                   {executable}")
-    for i, p in enumerate(path, start=1):
-        print(f"    Path-{str(i).zfill(2)}:                           {p}")
+    if "--debug" in argv:
+        print(f"  Command:                             {abspath(__file__)}")
+        print(f"  Python Executable:                   {executable}")
+        for i, p in enumerate(path, start=1):
+            print(f"    Path-{str(i).zfill(2)}:                           {p}")
 
 
 if __name__ == "__main__":
