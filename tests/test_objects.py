2548b2f18338f11aceb5613fc6aecf4760169554
pwt <1089749+pwt@users.noreply.github.com>
2023-09-26 17:03:56 +0100

    Improve list object detail test

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -8,14 +8,25 @@ from cli_test_helpers import shell
 
 class TestObjects:
     def test_object_details(self, tmp_path):
+        # The Expect script
         script_exp = """
         set timeout 10
-        spawn yd-list -oad -t ""
+        spawn yd-list -oad -n=pytest -t=""
         expect "press <Return> to cancel: "
         send "1\r"
         expect eof
         """
         p = tmp_path / "script.exp"
         p.write_text(script_exp)
+
+        # Ensure there's an object in the Object Store
+        result = shell(f"yd-upload -n=pytest -t='' {p}")
+        assert result.exit_code == 0
+
+        # Run the Expect script
         result = shell(f"expect {p}")
+        assert result.exit_code == 0
+
+        # Delete the object
+        result = shell(f"yd-delete -y -n=pytest -t='' {p}")
         assert result.exit_code == 0
