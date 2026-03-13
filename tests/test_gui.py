268051b9404d5c39e6d946932301be655fdbbf85
pwt <1089749+pwt@users.noreply.github.com>
2026-03-13 16:27:36 +0000

    Remove tests for primes and blender Qt GUIs

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -12,11 +12,3 @@ class TestGUIs:
     def test_generic_gui(self):
         result = shell(f"cd {DEMO_DIR}/yellow-gui && ./yellow-gui.py")
         assert result.exit_code == 0
-
-    def test_primes_gui(self):
-        result = shell(f"cd {DEMO_DIR}/primes/gui && ./primes-gui.py")
-        assert result.exit_code == 0
-
-    def test_blender_gui(self):
-        result = shell(f"cd {DEMO_DIR}/blender/gui && ./blender-gui.py")
-        assert result.exit_code == 0
